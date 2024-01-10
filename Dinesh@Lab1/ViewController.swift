800a578cc1c5f87518aee5aeb82d9d009787117c
Dinesh@Conestoga <reddydineshnaidu@gmail.com>
2024-01-10 15:44:54 -0500

    Initial Commit

---
+++ Dinesh@Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dinesh@Lab1
+//
+//  Created by user237706 on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
